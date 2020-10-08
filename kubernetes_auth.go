17361e644cedbd76b1d82615006fe8d71c52948d
Henning Surmeier <h.surmeier@mittwald.de>
2020-10-08 13:44:02 +0200

    implement requested changes from review

--- kubernetes_auth.go
+++ kubernetes_auth.go
@@ -81,10 +81,9 @@ func (k kubernetesAuth) Auth() (*AuthResponse, error) {
 	}
 
 	res := &AuthResponse{}
-	renew := false
 
 	err := k.Client.Write([]string{"v1", "auth", k.mountPoint, "login"}, conf, res, &RequestOptions{
-		RenewToken: &renew,
+		SkipRenewal: true,
 	})
 	if err != nil {
 		return nil, err
